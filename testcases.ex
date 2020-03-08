96fffa233edd67e14b4885f3bb1d7a8ea6bbd49b
Martin Mihaylov <martinmihaylov55@gmail.com>
2020-03-08 11:42:41 -0400

    Added new test case

--- testcases.ex
+++ testcases.ex
@@ -1,4 +1,2 @@
+IO.puts Poker.deal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 IO.puts Poker.deal([15, 33, 21, 4, 42, 19, 10, 52, 37, 29])
-
-IO.inspect Poker.convertHand([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
-IO.inspect Poker.convertHand([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
